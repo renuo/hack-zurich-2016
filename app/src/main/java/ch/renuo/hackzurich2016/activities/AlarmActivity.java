d09c49e4af0a7e30cb0021a2277aa28e27427bb7
Diego Steiner <diego.steiner@renuo.ch>
2016-09-17 16:10:59 +0200

    Cleanup imports

--- app/src/main/java/ch/renuo/hackzurich2016/activities/AlarmActivity.java
+++ app/src/main/java/ch/renuo/hackzurich2016/activities/AlarmActivity.java
@@ -1,21 +1,15 @@
 package ch.renuo.hackzurich2016.activities;
 
-import android.content.Context;
-import android.content.Intent;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
 import ch.renuo.hackzurich2016.R;
 import ch.renuo.hackzurich2016.alarms.AlarmController;
-import ch.renuo.hackzurich2016.alarms.StartAlarmReceiver;
 
 public class AlarmActivity extends AppCompatActivity {
 
