eed5f558d3af127d1547918b592f9400644949e5
Diego Steiner <diego.steiner@renuo.ch>
2016-09-17 10:39:10 +0200

    Add AlarmActivity

---
+++ app/src/main/java/ch/renuo/hackzurich2016/activities/AlarmActivity.java
@@ -0,0 +1,20 @@
+package ch.renuo.hackzurich2016.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TimePicker;
+
+import ch.renuo.hackzurich2016.R;
+
+public class AlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
+
+}
